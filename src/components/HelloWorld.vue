ea88e49ba082876b6ef40cbb512ca62d5a66e528
jackyscript <8930712+jackyscript@users.noreply.github.com>
2022-11-14 22:36:50 +0100

    Update application title

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,17 +1,19 @@
 <script setup lang="ts">
 defineProps<{
-  msg: string;
+  title: string;
+  type: string;
 }>();
 </script>
 
 <template>
   <div class="greetings">
-    <h1 class="green">{{ msg }}</h1>
+    <h1>{{ title }}</h1>
+    <h1 class="green">{{ type }}</h1>
     <h3>
-      You’ve successfully created a project with
+      A sample timekeeper application using
       <a href="https://vitejs.dev/" target="_blank" rel="noopener">Vite</a> +
       <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3</a>.
-      What's next?
+      How can I help you?
     </h3>
   </div>
 </template>
