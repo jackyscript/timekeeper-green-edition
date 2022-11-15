236383de70b4a461a38865f83d890f102d44b4f1
jackyscript <8930712+jackyscript@users.noreply.github.com>
2022-11-15 23:03:04 +0100

    Rename test case description

--- src/components/__tests__/TheIntroduction.spec.ts
+++ src/components/__tests__/TheIntroduction.spec.ts
@@ -3,7 +3,7 @@ import { describe, it, expect } from "vitest";
 import { mount } from "@vue/test-utils";
 import TheIntroduction from "../TheIntroduction.vue";
 
-describe("HelloWorld", () => {
+describe("TheIntroduction", () => {
   it("renders properly", () => {
     const wrapper = mount(TheIntroduction, {
       props: { title: "Timekeeper", type: "green edition" },
