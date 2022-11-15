2e7a5bdb57a368f0b7e8e2652a31fe37f20a4040
jackyscript <8930712+jackyscript@users.noreply.github.com>
2022-11-15 23:04:11 +0100

    Add activities select

--- src/views/TimeKeeper.vue
+++ src/views/TimeKeeper.vue
@@ -1,6 +1,15 @@
 <template>
   <div class="timekeeper">
     <h1>Welcome back, tell me about your day!</h1>
+
+    <label for="activities">What did you do?</label>
+
+    <select name="activities" id="activities">
+      <option value="work">Work</option>
+      <option value="vacation">Vacation</option>
+      <option value="recovery">Recovery</option>
+    </select>
+
     <label for="current-day">Current day:</label>
     <input type="date" id="current-day" name="timekeeper-start" />
   </div>
