c68499846d9d2092cdb1e28911ab85f19fcd1e09
jackyscript <8930712+jackyscript@users.noreply.github.com>
2022-12-02 00:34:18 +0100

    Basic add records implementation

--- src/components/TheTimeKeeper.vue
+++ src/components/TheTimeKeeper.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
-import { ref } from "vue";
+import { ref, type Ref } from "vue";
+import TimeRecords from "./TimeRecords.vue"
 
 const entryDate = ref("");
 const beginTime = ref("");
 const endTime = ref("");
+const activityType = ref("");
 const activityDescription = ref("");
 
 const currentDate: Date = new Date();
@@ -14,50 +16,82 @@ entryDate.value = currentDay;
 beginTime.value = currentTime;
 endTime.value = currentTime;
 
+const timeRecords: Ref<any> = ref([
+])
+
+function addTimeRecord() {
+  let currentRecord = ref(
+    {
+      entryDate: entryDate,
+      beginTime: beginTime,
+      endTime: endTime,
+      activityType: activityType,
+      activityDescription: activityDescription
+    }
+  )
+  timeRecords.value.push(currentRecord)
+}
 </script>
 
 <template>
+  <div class="flex-container">
+    <div class="flex-item">
+      <button title="Save this!" @click="addTimeRecord">💾</button>
+    </div>
+  </div>
   <h1>Welcome back, tell me about your day!</h1>
   <div class="timekeeper">
     <label for="activities">What did you do?</label>
-    <select name="activities" id="activities">
-      <option value="work">Work</option>
-      <option value="vacation">Vacation</option>
-      <option value="recovery">Recovery</option>
+    <select name="activities" id="activities" v-model="activityType">
+      <option>Work</option>
+      <option>Vacation</option>
+      <option>Recovery</option>
     </select>
 
     <label for="current-day">On which day:</label>
     <input type="date" id="current-day" name="timekeeper-day" v-model="entryDate" />
-    <p>Current day is: {{ entryDate == undefined ? currentDay : entryDate }}</p>
 
     <label for="start-time">When did you start?</label>
-    <input
-      type="time"
-      id="start-time"
-      name="timekeeper-start"
-      v-model="beginTime"
-    />
-    <p>Start time is: {{ beginTime == undefined ? currentTime : beginTime }}</p>
+    <input type="time" id="start-time" name="timekeeper-start" v-model="beginTime" />
 
     <label for="end-time">When did you finish?</label>
     <input type="time" id="end-time" name="timekeeper-end" v-model="endTime" />
-    <p>End time is: {{ endTime == undefined ? currentTime : endTime }}</p>
 
     <label for="notes">Tell me more about your activity:</label>
-    <textarea
-      id="notes"
-      name="notes-text"
-      rows="20"
+    <textarea id="notes" name="notes-text" rows="10"
       placeholder="Learned something new, did some exercise, read a chapter in a new book, cooked my favourite meal..."
-      maxlength="1000"
-      v-model="activityDescription"
-    ></textarea>
-    
-    <p>My activity was: {{ activityDescription }}</p>
+      maxlength="1000" v-model="activityDescription"></textarea>
   </div>
+
+  <TimeRecords :timeRecords="timeRecords"></TimeRecords>
+
 </template>
 
 <style scoped>
+.timekeeper-header {
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  grid-template-rows: repeat(1, 1fr);
+  column-gap: 300px;
+}
+
+.flex-container {
+  display: -ms-flexbox;
+  display: -webkit-flex;
+  display: flex;
+  flex-direction: row;
+  flex-wrap: nowrap;
+  justify-content: flex-end;
+  align-content: stretch;
+  align-items: flex-start;
+}
+
+.flex-item:nth-child(1) {
+  order: 0;
+  flex: 0 1 auto;
+  align-self: auto;
+}
+
 .timekeeper {
   display: grid;
   grid-template-columns: repeat(1, 1fr);
---
+++ src/components/TimeRecords.vue
@@ -0,0 +1,42 @@
+<script setup lang="ts">
+defineProps(["timeRecords"])
+</script>
+
+<template>
+  <h3>Summary:</h3>
+  <div class="summaryTableHeader">
+    <div>Type</div>
+    <div>Entry date</div>
+    <div>Start</div>
+    <div>End</div>
+    <div>Description</div>
+  </div>
+  <div class="summaryTable" v-for="record in timeRecords">
+    <div>{{ record.value.activityType }}</div>
+    <div>{{ record.value.entryDate }}</div>
+    <div>{{ record.value.beginTime }}</div>
+    <div>{{ record.value.endTime }}</div>
+    <div>{{ record.value.activityDescription }}</div>
+  </div>
+</template>
+
+<style scoped>
+.summaryTableHeader {
+  display: grid;
+  grid-template-columns: repeat(5, 1fr);
+  grid-template-rows: 1fr;
+  grid-column-gap: 0px;
+  grid-row-gap: 0px;
+}
+.summaryTableHeader div{
+  font-weight: bold;
+}
+
+.summaryTable {
+  display: grid;
+  grid-template-columns: repeat(5, 1fr);
+  grid-template-rows: 1fr;
+  grid-column-gap: 0px;
+  grid-row-gap: 0px;
+}
+</style>
