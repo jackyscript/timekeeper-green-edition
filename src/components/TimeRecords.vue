762c56d6a49c03ad4108ef15d83bd9d8d0fc7e8e
jackyscript <8930712+jackyscript@users.noreply.github.com>
2022-12-04 22:26:26 +0100

    Add css and title

--- src/components/TimeRecords.vue
+++ src/components/TimeRecords.vue
@@ -4,39 +4,51 @@ defineProps(["timeRecords"])
 
 <template>
   <h3>Summary:</h3>
-  <div class="summaryTableHeader">
+  <div class="summary-table-header">
     <div>Type</div>
     <div>Entry date</div>
     <div>Start</div>
     <div>End</div>
     <div>Description</div>
   </div>
-  <div class="summaryTable" v-for="record in timeRecords">
-    <div>{{ record.value.activityType }}</div>
-    <div>{{ record.value.entryDate }}</div>
-    <div>{{ record.value.beginTime }}</div>
-    <div>{{ record.value.endTime }}</div>
-    <div>{{ record.value.activityDescription }}</div>
+  <div class="summary-table">
+    <div class="timekeeper-entry" v-for="record in timeRecords" title="Click to edit this record">
+      <div>{{ record.value.activityType }}</div>
+      <div>{{ record.value.entryDate }}</div>
+      <div>{{ record.value.beginTime }}</div>
+      <div>{{ record.value.endTime }}</div>
+      <div>{{ record.value.activityDescription }}</div>
+    </div>
   </div>
 </template>
 
 <style scoped>
-.summaryTableHeader {
+.summary-table-header {
   display: grid;
   grid-template-columns: repeat(5, 1fr);
   grid-template-rows: 1fr;
   grid-column-gap: 0px;
   grid-row-gap: 0px;
 }
-.summaryTableHeader div{
+
+.summary-table-header div {
   font-weight: bold;
 }
 
-.summaryTable {
+.timekeeper-entry:nth-child(odd) {
+    background-color: gainsboro;
+}
+
+.timekeeper-entry {
   display: grid;
   grid-template-columns: repeat(5, 1fr);
   grid-template-rows: 1fr;
   grid-column-gap: 0px;
   grid-row-gap: 0px;
+}
+
+.timekeeper-entry:hover {
+  background-color: orange;
+  cursor: pointer;
 }
 </style>
